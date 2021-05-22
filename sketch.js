d45df222af3b1a17cb7f3eae67f30ff6a5fa39eb
Ardra2517 <74819734+Ardra2517@users.noreply.github.com>
2021-05-22 18:13:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -21,28 +21,38 @@ function draw() {
   push();
   rotate(scAngle);
   stroke(255,0,0);
-  arc(0,0,300,300,0,scAngle);
   strokeWeight(7);
   line(0,0,100,0);
   pop();
 
   push();
   rotate(hrAngle);
-  stroke(255,0,0);
+  stroke(255,255,255);
   strokeWeight(7);
-  line(0,0,100,0);
+  line(0,0,40,0);
   pop();
 
   push();
   rotate(mnAngle);
-  stroke(255,0,0);
+  stroke("blue");
   strokeWeight(7);
-  line(0,0,100,0);
+  line(0,0,70,0);
   pop();
 
+  stroke("red");
   strokeWeight(10);
   noFill();
+  arc(0,0,300,300,0,scAngle);
 
+  stroke("blue");
+  strokeWeight(10);
+  noFill();
+  arc(0,0,280,280,0,mnAngle);
+
+  stroke("white");
+  strokeWeight(10);
+  noFill();
+  arc(0,0,260,260,0,hrAngle);
 
   drawSprites();
 }
